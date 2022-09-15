b3b8f6a75e1dfd5eac197fd9be4971e3ee99190c
Антон <antonolegovich2017@icloud.com>
2022-09-15 18:24:27 +0600

    Курсач

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+    private static int coonter = 1;
+    private String FIO;
+    private int salary;
+    private int department;
+    private Integer id;
+
+    public Employee(String FIO, int salary, int department) {
+        this.FIO = FIO;
+        this.salary = salary;
+        this.department = department;
+        this.id=coonter++;
+    
+
+    }
+
+    public String getFIO() {
+
+        return FIO;
+    }
+
+    public int getSalary() {
+
+        return salary;
+    }
+
+    public int getDepartment() {
+
+        return department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return FIO + ", зарплата: " + salary + ", отдел: " + department + ", ID:" + id + ".";
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    private static Employee[] employees = new Employee[10];
+
+    public static void main(String[] args) {
+
+
+        Employee emp1 = new Employee(" Petrov Ivan Ivanovich", 35000, 1);
+        Employee emp2 = new Employee(" Ivanov Petr Ivanovich", 45000, 2);
+        Employee emp3 = new Employee(" Zaharov Vitya Yanovich", 56000, 5);
+        Employee emp4 = new Employee(" Kutuzov Dima Urich", 32999, 4);
+        Employee emp5 = new Employee(" Zhdanov Kolya Artemevich", 25000, 5);
+        Employee emp6 = new Employee(" Kutusova Zoya Evgenevna", 15000, 3);
+        Employee emp7 = new Employee(" Glupaya Inna Efremovna", 24000, 3);
+        Employee emp8 = new Employee(" Neganov Gora Igorevich", 17000, 3);
+        Employee emp9 = new Employee(" Sumov Geka Antonovich", 450000, 1);
+        Employee emp10 = new Employee(" Kakashkin Petr Dimonovich", 40000, 2);
+
+
+        employees[0] = emp1;
+        employees[1] = emp2;
+        employees[2]= emp3;
+        employees[3]=emp4;
+        employees[4]=emp5;
+        employees[5]=emp6;
+        employees[6]=emp7;
+        employees[7]=emp8;
+        employees[8]=emp9;
+        employees[9]=emp10;
+        calculateTotalSalary();
+        printEmployees();
+        printFullNames();
+        minSalary();
+        maxSalary();
+
+    }
+
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static Employee minSalary() {
+        Employee result = employees[0];
+        int minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+                result = employee;
+            }
+
+        }System.out.println(result);
+        return result;
+    }
+
+    public static Employee maxSalary(){
+        Employee result= employees[0];
+        int maxSalary= employees[0].getSalary();
+        for (Employee employee: employees){
+            if( employee.getSalary()> maxSalary){
+                maxSalary= employee.getSalary();
+                result=employee;
+            }
+
+        }System.out.println(result);
+        return result;
+    }
+
+    public static double calculateAverageSalary(){
+        return calculateTotalSalary()/(double)employees.length;
+    }
+
+    public static void printFullNames(){
+        for (Employee employee: employees){
+            System.out.println(employee.getFIO());
+        }
+    }
+}
